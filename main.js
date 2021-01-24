1470e9b9fa426e2820bd5f65c42b6ebb452832a2
garekun <76422585+garekun@users.noreply.github.com>
2021-01-24 17:37:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -18,9 +18,9 @@ if(iceCream === 'チョコレート') {
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
+      myImage.setAttribute ('src','neko-icon.png');
     } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
+      myImage.setAttribute ('src','firefox-logo.png');
     }
 }
 
